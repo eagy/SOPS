4672d1b6b98892d63bb29e620d2bf9f88651e994
Daniel Eagy <daniel.eagy@gmail.com>
2017-06-14 10:41:35 -0700

    update

--- src/client/Customer.java
+++ src/client/Customer.java
@@ -1,6 +1,17 @@
 package client;
 
+import address.Address;
+
 public class Customer {
-	String name; 
+	String name, contact, phone; 
+	Address address;
+	
+	public Customer() {
+		name = new String();
+		contact = new String();
+		phone = new String();
+		address = new Address();
+	}
+	
 	
 }
